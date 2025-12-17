16bc76831f9ce8192843c0a0c2c81e08ff4230e4
Salatick <Slava-Barkovsky@mail.ru>
2025-12-17 23:07:07 +0300

    New update

---
+++ project2/ex01/day01_ex01.sql
@@ -0,0 +1,9 @@
+SELECT object_name
+FROM (
+    SELECT name AS object_name, 1 AS ord
+    FROM person
+    UNION ALL
+    SELECT pizza_name AS object_name, 2 AS ord
+    FROM menu
+) t
+ORDER BY ord, object_name;
---
+++ project2/ex03/day01_ex03.sql
@@ -0,0 +1,10 @@
+SELECT 
+    po.order_date AS action_date,
+    po.person_id
+FROM person_order po
+INTERSECT
+SELECT 
+    pv.visit_date AS action_date,
+    pv.person_id
+FROM person_visits pv
+ORDER BY action_date ASC, person_id DESC;
